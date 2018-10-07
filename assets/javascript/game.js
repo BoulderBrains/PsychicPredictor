b3b9d0149bd75facfb44ed18de5246b2eb687277
Joshua <joshuadstarkey@gmail.com>
2018-10-06 19:32:23 -0700

    setting up starting variables and game reset

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var wins = 0;
+	var losses = 0;
+	var guessesRemaining = 5;
+	var guesses = [];
+
+});
+
+function reset() {
+	var wins = 0;
+	var losses = 0;
+	var guessesRemaining = 5;
+	var guesses = [];
+};
